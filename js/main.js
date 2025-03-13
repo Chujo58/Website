217a7912d50ef17f9c368f071bc9b7d81c15e878
Chujo58 <92330130+Chujo58@users.noreply.github.com>
2025-03-13 16:36:36 -0400

    changed routing and sticky navbar code

--- js/main.js
+++ js/main.js
@@ -1,46 +1,6 @@
 $(document).ready(function () {
     // Sticky navbar
-    // var aboveHeight = $("#navbar-wrapper").outerHeight();
-
-    // $(window).scroll(function () {
-    //     console.log($(window).scrollTop());
-    //     if ($(window).scrollTop() >= aboveHeight) {
-    //         $("#navbar-wrapper")
-    //             .addClass("fixed")
-    //             .css("top", "0")
-    //             .next()
-    //             .css("padding-top", "60px");
-    //     } else {
-    //         $("#navbar-wrapper")
-    //             .removeClass("fixed")
-    //             .css("top", "")
-    //             .next()
-    //             .css("padding-top", "0");
-    //     }
-    // });
-
     $("#navbar-wrapper").stick_in_parent();
-
-    // Some weird routing option using jQuery
-    // function showPage(pageID) {
-    //     $(".page").removeClass("active");
-    //     $("#" + pageID).addClass("active");
-    // }
-
-    // if (window.location.hash) {
-    //     showPage(window.location.hash.substring(1));
-    // } else {
-    //     showPage("home");
-    // }
-
-    // $(window).on("hashchange", function () {
-    //     showPage(window.location.hash.substring(1));
-    // });
-
-    // $("#utility-navbar a").click(function (event) {
-    //     event.preventDefault();
-    //     window.location.hash = this.hash;
-    // });
 });
 
 //Routing using AngularJS
